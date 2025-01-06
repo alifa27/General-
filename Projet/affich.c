f6eb39f9842162d70617245dc901cfa16378cb50
alifa27 <ali.fahs@isen.yncrea.fr>
2025-01-06 16:51:55 +0100

    Update affich.c

--- Projet/affich.c
+++ Projet/affich.c
@@ -16,32 +16,3 @@ void afficherVoitures(Voiture voitures[]) {
     }
 }
 
-// Fonction d'affichage pour le tableau des transactions
-void afficherTransactions(Transaction transactions[]) {
-    int taille = sizeof(transactions) / sizeof(transactions[0]);
-    printf("\nListe des Transactions :\n");
-    for (int i = 0; i < taille; i++) {
-        printf("ID Transaction: %d, ID Client: %d, Immatriculation Voiture: %s, Date: %s, Type: %s, Montant: %d€\n",
-               transactions[i].id_transaction,
-               transactions[i].id_client,
-               transactions[i].immatriculation_voiture,
-               transactions[i].date_transaction,
-               transactions[i].type_transaction,
-               transactions[i].montant);
-    }
-}
-
-// Fonction d'affichage pour le tableau des clients
-void afficherClients(Client clients[]) {
-    int taille = sizeof(clients) / sizeof(clients[0]);
-    printf("\nListe des Clients :\n");
-    for (int i = 0; i < taille; i++) {
-        printf("ID Client: %d, Nom: %s, Prénom: %s, Adresse: %s, Téléphone: %s, Email: %s, Location: %s, Achat: %s\n",
-               clients[i].id,
-               clients[i].nom,
-               clients[i].prenom,
-               clients[i].adresse,
-               clients[i].telephone,
-               clients[i].email,
-               clients[i].location ? "Oui" : "Non",
-               clients[i].achat ? "Oui" : "Non");
