274d49f5238226da67763066b4bc9b7949013d44
Titouan <titouan.lamorte@laposte.net>
2025-01-07 10:31:52 +0100

    Update global.c

--- global.c
+++ global.c
@@ -68,7 +68,63 @@ void interpreteur_commande(char commande[TAILLE_COM]) {
 
 
 
-// Fonction pour supprimer un client de la table
+// Fonction pour supprimer une ligne de la table client
+
+void suppression(Client clients[], int *taille) {
+
+    int id_suppression, index = -1;
+
+
+
+    printf("Quelle est l'ID de la ligne que vous voulez supprimer ? ");
+
+    scanf("%d", &id_suppression);
+
+
+
+    // Validation de l'ID
+
+    if (id_suppression < 1) {
+
+        printf("Erreur : l'ID doit être supérieur à 0.\n");
+
+        return;
+
+    }
+
+
+
+    // Rechercher l'index de la ligne à supprimer
+
+    for (int i = 0; i < *taille; i++) {
+
+        if (clients[i].id == id_suppression) {
+
+            index = i;
+
+            break;
+
+        }
+
+    }
+
+
+
+    // Si l'ID n'est pas trouvé
+
+    if (index == -1) {
+
+        printf("Erreur : Aucune ligne trouvée avec l'ID %d.\n", id_suppression);
+
+        return;
+
+    }
+
+    printf("La ligne avec l'ID %d a été supprimé avec succès.\n", id_suppression);
+
+}
+
+// Fonction pour supprimer une ligne de la table transaction
 
 void suppression(Client clients[], int *taille) {
 
@@ -124,3 +180,58 @@ void suppression(Client clients[], int *taille) {
 
 }
 
+// Fonction pour supprimer un client de la table
+
+void suppression(Client clients[], int *taille) {
+
+    int id_suppression, index = -1;
+
+
+
+    printf("Quelle est l'ID du client que vous voulez supprimer ? ");
+
+    scanf("%d", &id_suppression);
+
+
+
+    // Validation de l'ID
+
+    if (id_suppression < 1) {
+
+        printf("Erreur : l'ID doit être supérieur à 0.\n");
+
+        return;
+
+    }
+
+
+
+    // Rechercher l'index du client à supprimer
+
+    for (int i = 0; i < *taille; i++) {
+
+        if (clients[i].id == id_suppression) {
+
+            index = i;
+
+            break;
+
+        }
+
+    }
+
+
+
+    // Si l'ID n'est pas trouvé
+
+    if (index == -1) {
+
+        printf("Erreur : Aucun client trouvé avec l'ID %d.\n", id_suppression);
+
+        return;
+
+    }
+
+    printf("Le client avec l'ID %d a été supprimé avec succès.\n", id_suppression);
+
+}
